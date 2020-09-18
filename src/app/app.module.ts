af17b32e7ba9f09706793b3e3fa9f74494621b5b
Joseph Lloyd <lloyd.joseph89@gmail.com>
2020-09-18 15:52:05 +0200

    add new interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: "AIzaSyCJQg35PURs_WiwnlkMDEWgIfAFkTozDIM",
+  authDomain: "notes-app-ce887.firebaseapp.com",
+  databaseURL: "https://notes-app-ce887.firebaseio.com",
+  projectId: "notes-app-ce887",
+  storageBucket: "notes-app-ce887.appspot.com",
+  messagingSenderId: "117451141873",
+  appId: "1:117451141873:web:3364ffac2761260815ef50",
+  measurementId: "G-FLL0139SQJ"
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // 3. Initialize
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {}
+
